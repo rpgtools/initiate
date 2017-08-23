309269130b7819e1072c256bb7ccace0edea6a4e
drewandrew <andrewcdixon673@gmail.com>
2017-08-23 18:54:49 -0400

    moves grid-layouts to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 import {counters} from './counters';
 import {creatures} from './creatures';
 import {stateManagement} from './stateManagement';
+import {display} from './display'
 
 export default function (state, action) {
   return stateManagement(state, action, combineReducers({
     counters,
-    creatures
+    creatures,
+    display
   }));
 }
