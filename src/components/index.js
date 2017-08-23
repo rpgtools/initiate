de333c1918a42f7b419abe6a512c1c81e1cde705
Josh Lee <josh@daftlabs.com>
2017-08-23 16:02:16 -0400

    adds a webframe component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import CreatureList from './CreatureList';
 import StateManagement from './StateManagement';
+import WebFrame from './WebFrame';
 
 export {
   CreatureList,
-  StateManagement
+  StateManagement,
+  WebFrame,
 }
