13bbd2c74ba73ef003475c79dcf4bc93e7f62885
drewandrew <andrewcdixon673@gmail.com>
2017-08-23 14:29:28 -0400

    adds grid-layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
-import {CreatureList, StateManagement} from './components';
+import GridLayout from './components/GridLayout';
 import './App.css';
+import '../node_modules/react-grid-layout/css/styles.css';
+import '../node_modules/react-resizable/css/styles.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <StateManagement/>
-        <CreatureList/>
+        <GridLayout />
       </div>
     );
   }
