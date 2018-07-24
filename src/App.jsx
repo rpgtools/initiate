3944c45a407508eb7397e41fc99d7deb1b5470e9
drewandrew <andrewcdixon673@gmail.com>
2018-07-24 08:52:20 -0400

    WIP Directory structure - Add StateManagement to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
+
 import Initiative from './components/Initiative';
+import StateManagement from './components/StateManagement';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <StateManagement />
         <Initiative />
       </div>
     );
