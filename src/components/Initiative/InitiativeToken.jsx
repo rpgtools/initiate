44eb537501df60fc5dd5f10c3d09bed873d755b4
drewandrew <andrewcdixon673@gmail.com>
2018-07-24 09:20:19 -0400

    Make InitiativeToken into functional component

--- src/components/Initiative/InitiativeToken.jsx
+++ src/components/Initiative/InitiativeToken.jsx
@@ -3,51 +3,54 @@ import React from 'react';
 import Counter from '../Counter';
 import Button from '../Button';
 
-class InitiativeToken extends React.Component {
-
-  handleCreateCounter = label => {
-    this.props.createCounter(this.props.creature.id, label);
+const InitiativeToken = ({
+  creature,
+  createCounter,
+  deleteCounter,
+  updateCounter,
+}) => {
+  const handleCreateCounter = label => {
+    createCounter(creature.id, label);
   };
 
-  handleDeleteCounter = counterIndex => () => {
-    this.props.deleteCounter(this.props.creature.id, counterIndex);
+  const handleDeleteCounter = counterIndex => () => {
+    deleteCounter(creature.id, counterIndex);
   };
 
-  handleUpdateCounter = counterIndex => value => {
-    this.props.updateCounter(this.props.creature.id, counterIndex, value);
+  const handleUpdateCounter = counterIndex => value => {
+    updateCounter(creature.id, counterIndex, value);
   };
 
-  render () {
-    const { creature } = this.props;
-    return (
-      <div className="initiative-token">
-        <h2 className="initiative-token_title">{creature.name}</h2>
-        <div className="initiative-token_left">
-          {creature.counters.map((counter, index) =>
-            <Counter
-              key={index}
-              label={counter.label}
-              value={counter.value}
-              onUpdateValue={this.handleUpdateCounter(index)}
-              onClickDelete={this.handleDeleteCounter(index)}
+  return (
+    <div className="initiative-token">
+      <h2 className="initiative-token_title">{creature.name}</h2>
+      <div className="initiative-token_left">
+        {creature.counters.map((counter, index) =>
+          <Counter
+            key={index}
+            label={counter.label}
+            value={counter.value}
+            onUpdateValue={handleUpdateCounter(index)}
+            onClickDelete={handleDeleteCounter(index)}
             />
-          )}
-        </div>
-        <div className="initiative-token_right">
-          <Button
-            buttonLabel="New Counter"
-            onSubmit={this.handleCreateCounter}
-          />
-          <Button
-            buttonLabel="Edit Creature"
-            onSubmit={console.log('EDIT CREATURE')}
-          />
-          <Button
-           buttonLabel="Delete Creature"
-           onSubmit={this.handleDeleteCounter}
-          />
-        </div>
+        )}
       </div>
-    );
-  }
-}
+      <div className="initiative-token_right">
+        <Button
+          buttonLabel="New Counter"
+          onSubmit={handleCreateCounter}
+          />
+        <Button
+          buttonLabel="Edit Creature"
+          onSubmit={console.log('EDIT CREATURE')}
+          />
+        <Button
+          buttonLabel="Delete Creature"
+          onSubmit={handleDeleteCounter}
+          />
+      </div>
+    </div>
+  );
+};
+
+export default InitiativeToken;
