dd1e7c8bd2c2bdc882a0dce6af1ee342fcbc8e7b
drewandrew <andrewcdixon673@gmail.com>
2018-09-04 11:40:26 -0400

    Comment out Login stuff in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,36 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 
 import Initiative from './components/Initiative';
 // import CreatureDetails from './components/CreatureDetails';
 // import InitiativeButtonsWidget from './components/Initiative/InitiativeButtons';
-import { userSelector } from './services/api/auth/selectors';
+// import { userSelector } from './services/api/auth/selectors';
 import './styles/index.scss';
 
 class App extends Component {
   componentDidMount() {
-
+    //TODO: attempt login
   }
   render () {
-    const { user } = this.props;
-    if (document.cookie === '') { console.log('EMPTY')}
     return (
       <div>
-        {/*user.token && user.token === document.cookie*/}
-        {true  ? (
-          <main className="main">
-            <Initiative />
-            {/*}<div className="temporary-right-side">
-              <InitiativeButtonsWidget />
-              <CreatureDetails />
-              <button onClick={() => { document.cookie = 'token=;'; this.forceUpdate()}}> LOGOUT</button>
-              <button onClick={() => this.props.postCampaign() }>POST Campaign</button>
-            </div>*/}
-          </main>
-        ) : (
-          <main className="main">
-            <a href={'http://localhost:3001/auth/google'}>LOGIN </a>
-            <button onClick={this.props.getUser}>Set user in redux</button>
-          </main>
-          )
-        }
+        <main className="main">
+          <Initiative />
+        </main>
       </div>
     );
   }
 }
 
-const mapDispatch = dispatch => ({
-  getUser: () => dispatch({ type: 'GET_USER' }),
-  postCampaign: () => dispatch({ type: 'campaign/POST' }),
-
-});
-
-const mapState = state => ({
-  user: userSelector(state),
-});
-
-export default connect(mapState, mapDispatch)(App);
+// const mapDispatch = dispatch => ({
+//   getUser: () => dispatch({ type: 'GET_USER' }),
+//   postCampaign: () => dispatch({ type: 'campaign/POST' }),
+//
+// });
+//
+// const mapState = state => ({
+//   user: userSelector(state),
+// });
+//
+// export default connect(mapState, mapDispatch)(App);
+export default App;
